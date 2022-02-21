4779b91c130b31b282a77ccc232e1fe8c0dfddf2
NikBel3476 <nikita.belkin3476@gmail.com>
2022-02-21 22:21:07 +0400

    move api's appid to .env

--- src/wheatherApi/index.ts
+++ src/wheatherApi/index.ts
@@ -8,7 +8,7 @@ const $geocoding = axios.create({
     baseURL: "http://api.openweathermap.org/geo/1.0",
     timeout: 2000,
     params: {
-        appid: "2d70458a675762814eabff13e15fb4ae"
+        appid: process.env.API_APP_ID
     }
 });
 
