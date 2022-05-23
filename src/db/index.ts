3f7c581c1b5ecd55291af2072e338960445e0428
NikBel3476 <nikita.belkin3476@gmail.com>
2022-05-23 21:30:35 +0400

    feature: added editor config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,122 +1,130 @@
 import oracledb, {
-  BindParameters,
-  ExecuteManyOptions,
-  ExecuteOptions,
+	BindParameters,
+	ExecuteManyOptions,
+	ExecuteOptions,
 } from "oracledb";
 import { dbconfig } from "./dbconfig";
 import { City, CityWeatherInfo } from "../@types/dbmodels";
 import { WindRoseDirectionStats } from "../@types/WindRose";
 
 class Database {
-  async initialize() {
-    await oracledb.createPool(dbconfig.pool);
-  }
+	async initialize() {
+		try {
+			await oracledb.createPool(dbconfig.pool);
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-  async closePool() {
-    await oracledb.getPool().close(5);
-  }
+	async closePool() {
+		try {
+			await oracledb.getPool().close(5);
+		} catch (e) {
+			console.error(e);
+		}
+	}
 
-  private async execute<T>(
-    sql: string,
-    bindParams: BindParameters,
-    options: ExecuteOptions
-  ) {
-    let connection;
+	private async execute<T>(
+		sql: string,
+		bindParams: BindParameters,
+		options: ExecuteOptions
+	) {
+		let connection;
 
-    try {
-      connection = await oracledb.getConnection();
-      return await connection.execute<T>(sql, bindParams, options);
-    } catch (e) {
-      console.error(e);
-    } finally {
-      if (connection) {
-        try {
-          await connection.close();
-        } catch (e) {
-          console.error(e);
-        }
-      }
-    }
-  }
+		try {
+			connection = await oracledb.getConnection();
+			return await connection.execute<T>(sql, bindParams, options);
+		} catch (e) {
+			console.error(e);
+		} finally {
+			if (connection) {
+				try {
+					await connection.close();
+				} catch (e) {
+					console.error(e);
+				}
+			}
+		}
+	}
 
-  private async executeMany<T>(
-    sql: string,
-    binds: BindParameters[],
-    options: ExecuteManyOptions
-  ) {
-    let connection;
+	private async executeMany<T>(
+		sql: string,
+		binds: BindParameters[],
+		options: ExecuteManyOptions
+	) {
+		let connection;
 
-    try {
-      connection = await oracledb.getConnection();
-      return await connection.executeMany<T[]>(sql, binds, options);
-    } catch (e) {
-      console.error(e);
-    } finally {
-      if (connection) {
-        try {
-          await connection.close();
-        } catch (e) {
-          console.error(e);
-        }
-      }
-    }
-  }
+		try {
+			connection = await oracledb.getConnection();
+			return await connection.executeMany<T[]>(sql, binds, options);
+		} catch (e) {
+			console.error(e);
+		} finally {
+			if (connection) {
+				try {
+					await connection.close();
+				} catch (e) {
+					console.error(e);
+				}
+			}
+		}
+	}
 
-  async cityGetByName(cityName: string) {
-    const data = await this.execute<City>(
-      `select ID "id", NAME "name" from CITY where NAME = :cityName`,
-      [cityName],
-      {
-        outFormat: oracledb.OUT_FORMAT_OBJECT,
-        maxRows: 1,
-      }
-    );
-    if (data?.rows) return data.rows[0];
-    return null;
-  }
+	async cityGetByName(cityName: string) {
+		const data = await this.execute<City>(
+			`select ID "id", NAME "name" from CITY where NAME = :cityName`,
+			[cityName],
+			{
+				outFormat: oracledb.OUT_FORMAT_OBJECT,
+				maxRows: 1,
+			}
+		);
+		if (data?.rows) return data.rows[0];
+		return null;
+	}
 
-  async cityGetAll() {
-    const data = await this.execute<City>(
-      `select ID "id", NAME "name" from CITY`,
-      [],
-      {
-        outFormat: oracledb.OUT_FORMAT_OBJECT,
-      }
-    );
-    return data?.rows || null;
-  }
+	async cityGetAll() {
+		const data = await this.execute<City>(
+			`select ID "id", NAME "name" from CITY`,
+			[],
+			{
+				outFormat: oracledb.OUT_FORMAT_OBJECT,
+			}
+		);
+		return data?.rows || null;
+	}
 
-  async cityAddOne(name: string) {
-    const data = await this.execute<City>(
-      `
+	async cityAddOne(name: string) {
+		const data = await this.execute<City>(
+			`
       insert into CITY
-        (NAME) 
+        (NAME)
       values (:cityName)
       returning
         ID, NAME into :id, :name
       `,
-      {
-        cityName: name,
-        id: { type: oracledb.NUMBER, dir: oracledb.BIND_OUT },
-        name: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
-      },
-      {
-        autoCommit: true,
-        outFormat: oracledb.OUT_FORMAT_OBJECT,
-      }
-    );
-    if (data?.outBinds) {
-      const city: City = {
-        ...data?.outBinds,
-      };
-      return city;
-    }
-    return null;
-  }
+			{
+				cityName: name,
+				id: { type: oracledb.NUMBER, dir: oracledb.BIND_OUT },
+				name: { type: oracledb.DB_TYPE_VARCHAR, dir: oracledb.BIND_OUT },
+			},
+			{
+				autoCommit: true,
+				outFormat: oracledb.OUT_FORMAT_OBJECT,
+			}
+		);
+		if (data?.outBinds) {
+			const city: City = {
+				...data?.outBinds,
+			};
+			return city;
+		}
+		return null;
+	}
 
-  async cityUpdateOne(city: City) {
-    const data = await this.execute(
-      `
+	async cityUpdateOne(city: City) {
+		const data = await this.execute(
+			`
       update
         CITY
       set
@@ -124,45 +132,45 @@ class Database {
       where
         ID = :id
       `,
-      city,
-      {
-        outFormat: oracledb.OUT_FORMAT_OBJECT,
-        autoCommit: true,
-      }
-    );
+			city,
+			{
+				outFormat: oracledb.OUT_FORMAT_OBJECT,
+				autoCommit: true,
+			}
+		);
 
-    return data?.rowsAffected || null;
-  }
+		return data?.rowsAffected || null;
+	}
 
-  async cityDeleteOne(id: number) {
-    const data = await this.execute(
-      `
+	async cityDeleteOne(id: number) {
+		const data = await this.execute(
+			`
       delete from
         CITY
       where
         ID = :id
       `,
-      {
-        id,
-      },
-      {
-        outFormat: oracledb.OUT_FORMAT_OBJECT,
-        autoCommit: true,
-      }
-    );
+			{
+				id,
+			},
+			{
+				outFormat: oracledb.OUT_FORMAT_OBJECT,
+				autoCommit: true,
+			}
+		);
 
-    return data?.rowsAffected || null;
-  }
+		return data?.rowsAffected || null;
+	}
 
-  async weatherGetWindRoseStats(
-    cityId: number,
-    startDatetime: string,
-    endDatetime: string
-  ) {
-    const weatherData = await this.execute<
-      WindRoseDirectionStats & { count: number }
-    >(
-      `
+	async weatherGetWindRoseStats(
+		cityId: number,
+		startDatetime: string,
+		endDatetime: string
+	) {
+		const weatherData = await this.execute<
+			WindRoseDirectionStats & { count: number }
+		>(
+			`
       (select
           round(avg(WIND_SPEED), 3) "windSpeed",
           round(avg(WIND_GUST), 3) "windGust",
@@ -277,21 +285,21 @@ class Database {
       order by
         "count"
       `,
-      {
-        cityId,
-        startDatetime,
-        endDatetime,
-      },
-      {
-        outFormat: oracledb.OUT_FORMAT_OBJECT,
-      }
-    );
-    return weatherData?.rows || null;
-  }
+			{
+				cityId,
+				startDatetime,
+				endDatetime,
+			},
+			{
+				outFormat: oracledb.OUT_FORMAT_OBJECT,
+			}
+		);
+		return weatherData?.rows || null;
+	}
 
-  async weatherGetMany(cityId: number, startDatetime: Date, endDatetime: Date) {
-    const weatherData = await this.execute<CityWeatherInfo>(
-      `
+	async weatherGetMany(cityId: number, startDatetime: Date, endDatetime: Date) {
+		const weatherData = await this.execute<CityWeatherInfo>(
+			`
       select
         CITY_ID "cityId",
         DATETIME "datetime",
@@ -305,22 +313,22 @@ class Database {
       where
         CITY_ID = :cityId and DATETIME between :startDatetime and :endDatetime
       `,
-      {
-        cityId,
-        startDatetime,
-        endDatetime,
-      },
-      {
-        outFormat: oracledb.OUT_FORMAT_OBJECT,
-      }
-    );
+			{
+				cityId,
+				startDatetime,
+				endDatetime,
+			},
+			{
+				outFormat: oracledb.OUT_FORMAT_OBJECT,
+			}
+		);
 
-    return weatherData?.rows || null;
-  }
+		return weatherData?.rows || null;
+	}
 
-  async weatherAddMany(weatherData: CityWeatherInfo[]) {
-    return await this.executeMany(
-      `
+	async weatherAddMany(weatherData: CityWeatherInfo[]) {
+		return await this.executeMany(
+			`
       merge into
         WEATHER
       using DUAL
@@ -329,27 +337,27 @@ class Database {
           insert (DATETIME, WIND_SPEED, WIND_DIRECTION, WIND_GUST, CITY_ID)
           values (:datetime, :windSpeed, :windDirection, :windGust, :cityId)
       `,
-      weatherData,
-      {
-        autoCommit: true,
-        bindDefs: {
-          cityId: { type: oracledb.NUMBER },
-          datetime: { type: oracledb.DATE },
-          windSpeed: { type: oracledb.NUMBER },
-          windDirection: { type: oracledb.NUMBER },
-          windGust: { type: oracledb.NUMBER },
-        },
-      }
-    );
-  }
+			weatherData,
+			{
+				autoCommit: true,
+				bindDefs: {
+					cityId: { type: oracledb.NUMBER },
+					datetime: { type: oracledb.DATE },
+					windSpeed: { type: oracledb.NUMBER },
+					windDirection: { type: oracledb.NUMBER },
+					windGust: { type: oracledb.NUMBER },
+				},
+			}
+		);
+	}
 
-  async getNotExistingDates(
-    cityId: number,
-    startDateISO: string,
-    endDateISO: string
-  ) {
-    const data = await this.execute<{ MAX_DATETIME: Date; MIN_DATETIME: Date }>(
-      `
+	async getNotExistingDates(
+		cityId: number,
+		startDateISO: string,
+		endDateISO: string
+	) {
+		const data = await this.execute<{ MAX_DATETIME: Date; MIN_DATETIME: Date }>(
+			`
       select
         max(datetime) max_datetime,
         min(datetime) min_datetime
@@ -370,17 +378,17 @@ class Database {
           datetime between to_date(:startDatetime, 'yyyy-mm-dd"T"HH24:MI:SS') and
           to_date(:endDatetime, 'yyyy-mm-dd"T"HH24:MI:SS'))
       `,
-      {
-        cityId,
-        startDatetime: startDateISO,
-        endDatetime: endDateISO,
-      },
-      {
-        outFormat: oracledb.OUT_FORMAT_OBJECT,
-      }
-    );
-    return data?.rows || null;
-  }
+			{
+				cityId,
+				startDatetime: startDateISO,
+				endDatetime: endDateISO,
+			},
+			{
+				outFormat: oracledb.OUT_FORMAT_OBJECT,
+			}
+		);
+		return data?.rows || null;
+	}
 }
 
 export const db = new Database();
