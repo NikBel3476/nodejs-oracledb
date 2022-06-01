e4616d95dc12acb64fabbc26ba9837e5475ed514
NikBel3476 <nikita.belkin3476@gmail.com>
2022-06-01 18:30:58 +0400

    fix: delete .gitmodules

--- src/controllers/weatherController.ts
+++ src/controllers/weatherController.ts
@@ -7,58 +7,62 @@ import { CityWeatherInfo } from "../@types/dbmodels";
 import { CityWeatherAPI } from "../@types/weatherAPI";
 
 class WeatherController {
-  async getWindStats(req: Request, res: Response, next: NextFunction) {
-    const { startDatetime, endDatetime } = req.params;
-    const startDate = moment(startDatetime);
-    const endDate = moment(endDatetime);
+	async getWindStats(req: Request, res: Response, next: NextFunction) {
+		const { startDatetime, endDatetime } = req.params;
+		const startDate = moment(startDatetime);
+		const endDate = moment(endDatetime);
 
-    try {
-      const notExistingDates = await db.getNotExistingDates(
-        req.city.id,
-        startDate.format("YYYY-MM-DDTHH:mm:ss"),
-        endDate.format("YYYY-MM-DDTHH:mm:ss")
-      );
+		if (startDate > endDate) {
+			return next(ApiError.badRequest("Start date is greater than end date"));
+		}
 
-      if (
-        notExistingDates &&
-        notExistingDates[0].MIN_DATETIME &&
-        notExistingDates[0].MAX_DATETIME
-      ) {
-        const startDatetimeToFetch = moment(
-          notExistingDates[0].MIN_DATETIME
-        ).format("YYYY-MM-DD");
-        const endDatetimeToFetch = moment(
-          notExistingDates[0].MAX_DATETIME
-        ).format("YYYY-MM-DD");
-        const apiRes = await $weatherApi.get<CityWeatherAPI>(
-          `/timeline/${req.city.name}/${startDatetimeToFetch}/${endDatetimeToFetch}`
-        );
-        if (apiRes.data) {
-          const weatherInfo: CityWeatherInfo[] = [];
-          apiRes.data.days.forEach((day) =>
-            day.hours.forEach((hour) => {
-              weatherInfo.push({
-                cityId: req.city.id,
-                datetime: moment(`${day.datetime} ${hour.datetime}`).toDate(),
-                windSpeed: hour.windspeed,
-                windDirection: hour.winddir,
-                windGust: hour.windgust,
-              });
-            })
-          );
-          await db.weatherAddMany(weatherInfo);
-        }
-      }
-      const windRoseStats = await db.weatherGetWindRoseStats(
-        req.city.id,
-        startDate.format("YYYY-MM-DDTHH:mm:ss"),
-        endDate.format("YYYY-MM-DDTHH:mm:ss")
-      );
-      return res.json(windRoseStats);
-    } catch (e) {
-      return next(ApiError.internal("Error on third party API request"));
-    }
-  }
+		try {
+			const notExistingDates = await db.getNotExistingDates(
+				req.city.id,
+				startDate.format("YYYY-MM-DDTHH:mm:ss"),
+				endDate.format("YYYY-MM-DDTHH:mm:ss")
+			);
+
+			if (
+				notExistingDates &&
+				notExistingDates[0].MIN_DATETIME &&
+				notExistingDates[0].MAX_DATETIME
+			) {
+				const startDatetimeToFetch = moment(
+					notExistingDates[0].MIN_DATETIME
+				).format("YYYY-MM-DD");
+				const endDatetimeToFetch = moment(
+					notExistingDates[0].MAX_DATETIME
+				).format("YYYY-MM-DD");
+				const apiRes = await $weatherApi.get<CityWeatherAPI>(
+					`/timeline/${req.city.name}/${startDatetimeToFetch}/${endDatetimeToFetch}`
+				);
+				if (apiRes.data) {
+					const weatherInfo: CityWeatherInfo[] = [];
+					apiRes.data.days.forEach((day) =>
+						day.hours.forEach((hour) => {
+							weatherInfo.push({
+								cityId: req.city.id,
+								datetime: moment(`${day.datetime} ${hour.datetime}`).toDate(),
+								windSpeed: hour.windspeed,
+								windDirection: hour.winddir,
+								windGust: hour.windgust,
+							});
+						})
+					);
+					await db.weatherAddMany(weatherInfo);
+				}
+			}
+			const windRoseStats = await db.weatherGetWindRoseStats(
+				req.city.id,
+				startDate.format("YYYY-MM-DDTHH:mm:ss"),
+				endDate.format("YYYY-MM-DDTHH:mm:ss")
+			);
+			return res.json(windRoseStats);
+		} catch (e) {
+			return next(ApiError.internal("Error on third party API request"));
+		}
+	}
 }
 
 export default new WeatherController();
